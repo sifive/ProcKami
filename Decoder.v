4c3b8b8c7ad96cb8c35ce1cfc71a4486d16dcca0
Kade Phillips <kade.phillips@sifive.com>
2018-06-28 13:31:53 -0700

    minor reordering and updated TODO

--- Decoder.v
+++ Decoder.v
@@ -42,75 +42,75 @@ Section Decoder.
 
 (* "Minor" Opcodes i.e. funct3 *)
 
-    Definition Minor_ADDI      := WO~0~0~0.
-    Definition Minor_SLLI      := WO~0~0~1.
-    Definition Minor_SLTI      := WO~0~1~0.
-    Definition Minor_SLTIU     := WO~0~1~1.
-    Definition Minor_XORI      := WO~1~0~0.
-    Definition Minor_SRLI      := WO~1~0~1.
-    Definition Minor_SRAI      := WO~1~0~1.
-    Definition Minor_ORI       := WO~1~1~0.
-    Definition Minor_ANDI      := WO~1~1~1.
+    Definition Minor_ADDI    := WO~0~0~0.
+    Definition Minor_SLLI    := WO~0~0~1.
+    Definition Minor_SLTI    := WO~0~1~0.
+    Definition Minor_SLTIU   := WO~0~1~1.
+    Definition Minor_XORI    := WO~1~0~0.
+    Definition Minor_SRLI    := WO~1~0~1.
+    Definition Minor_SRAI    := WO~1~0~1. (* same as Minor_SRLI *)
+    Definition Minor_ORI     := WO~1~1~0.
+    Definition Minor_ANDI    := WO~1~1~1.
 
-    Definition Minor_ADD       := WO~0~0~0.
-    Definition Minor_SUB       := WO~0~0~0.
-    Definition Minor_SLL       := WO~0~0~1.
-    Definition Minor_SLT       := WO~0~1~0.
-    Definition Minor_SLTU      := WO~0~1~1.
-    Definition Minor_XOR       := WO~1~0~0.
-    Definition Minor_SRL       := WO~1~0~1.
-    Definition Minor_SRA       := WO~1~0~1.
-    Definition Minor_OR        := WO~1~1~0.
-    Definition Minor_AND       := WO~1~1~1.
+    Definition Minor_ADD     := WO~0~0~0.
+    Definition Minor_SUB     := WO~0~0~0. (* same as Minor_ADD *)
+    Definition Minor_SLL     := WO~0~0~1.
+    Definition Minor_SLT     := WO~0~1~0.
+    Definition Minor_SLTU    := WO~0~1~1.
+    Definition Minor_XOR     := WO~1~0~0.
+    Definition Minor_SRL     := WO~1~0~1.
+    Definition Minor_SRA     := WO~1~0~1. (* same as Minor_SRL  *)
+    Definition Minor_OR      := WO~1~1~0.
+    Definition Minor_AND     := WO~1~1~1.
 
-    Definition Minor_BEQ       := WO~0~0~0.
-    Definition Minor_BNE       := WO~0~0~1.
-    Definition Unused_B1       := WO~0~1~0.
-    Definition Unused_B2       := WO~0~1~1.
-    Definition Minor_BLT       := WO~1~0~0.
-    Definition Minor_BGE       := WO~1~0~1.
-    Definition Minor_BLTU      := WO~1~1~0.
-    Definition Minor_BGEU      := WO~1~1~1.
+    Definition Minor_BEQ     := WO~0~0~0.
+    Definition Minor_BNE     := WO~0~0~1.
+    Definition Unused_B1     := WO~0~1~0.
+    Definition Unused_B2     := WO~0~1~1.
+    Definition Minor_BLT     := WO~1~0~0.
+    Definition Minor_BGE     := WO~1~0~1.
+    Definition Minor_BLTU    := WO~1~1~0.
+    Definition Minor_BGEU    := WO~1~1~1.
 
-    Definition Minor_LB        := WO~0~0~0.
-    Definition Minor_LH        := WO~0~0~1.
-    Definition Minor_LW        := WO~0~1~0.
-    Definition Minor_LD        := WO~0~1~1. (* RV64 only *)
-    Definition Minor_LBU       := WO~1~0~0.
-    Definition Minor_LHU       := WO~1~0~1.
-    Definition Minor_LWU       := WO~1~1~0. (* RV64 only *)
-    Definition Unused_L1       := WO~1~1~1.
+    Definition Minor_LB      := WO~0~0~0.
+    Definition Minor_LH      := WO~0~0~1.
+    Definition Minor_LW      := WO~0~1~0.
+    Definition Minor_LD      := WO~0~1~1. (* RV64 only *)
+    Definition Minor_LBU     := WO~1~0~0.
+    Definition Minor_LHU     := WO~1~0~1.
+    Definition Minor_LWU     := WO~1~1~0. (* RV64 only *)
+    Definition Unused_L1     := WO~1~1~1.
 
-    Definition Minor_SB        := WO~0~0~0.
-    Definition Minor_SH        := WO~0~0~1.
-    Definition Minor_SW        := WO~0~1~0.
-    Definition Minor_SD        := WO~0~1~1. (* RV64 only *)
-    Definition Unused_S1       := WO~1~0~0.
-    Definition Unused_S2       := WO~1~0~1.
-    Definition Unused_S3       := WO~1~1~0.
-    Definition Unused_S4       := WO~1~1~1.
+    Definition Minor_SB      := WO~0~0~0.
+    Definition Minor_SH      := WO~0~0~1.
+    Definition Minor_SW      := WO~0~1~0.
+    Definition Minor_SD      := WO~0~1~1. (* RV64 only *)
+    Definition Unused_S1     := WO~1~0~0.
+    Definition Unused_S2     := WO~1~0~1.
+    Definition Unused_S3     := WO~1~1~0.
+    Definition Unused_S4     := WO~1~1~1.
 
-    Definition Minor_ECALL     := WO~0~0~0.
-    Definition Minor_EBREAK    := WO~0~0~0.
-    Definition Minor_CSRRW     := WO~0~0~1.
-    Definition Minor_CSRRS     := WO~0~1~0.
-    Definition Minor_CSRRC     := WO~0~1~1.
-    Definition Unused_C1       := WO~1~0~0.
-    Definition Minor_CSRRWI    := WO~1~0~1.
-    Definition Minor_CSRRSI    := WO~1~1~0.
-    Definition Minor_CSRRCI    := WO~1~1~1.
+    Definition Minor_ECALL   := WO~0~0~0.
+    Definition Minor_EBREAK  := WO~0~0~0. (* same as Minor_ECALL *)
+    Definition Minor_CSRRW   := WO~0~0~1.
+    Definition Minor_CSRRS   := WO~0~1~0.
+    Definition Minor_CSRRC   := WO~0~1~1.
+    Definition Unused_C1     := WO~1~0~0.
+    Definition Minor_CSRRWI  := WO~1~0~1.
+    Definition Minor_CSRRSI  := WO~1~1~0.
+    Definition Minor_CSRRCI  := WO~1~1~1.
 
-    Definition Minor_FENCE     := WO~0~0~0.
-    Definition Minor_FENCE_I   := WO~0~0~1.
-    Definition Unused_F1       := WO~0~1~0.
-    Definition Unused_F2       := WO~0~1~1.
-    Definition Unused_F3       := WO~1~0~0.
-    Definition Unused_F4       := WO~1~0~1.
-    Definition Unused_F5       := WO~1~1~0.
-    Definition Unused_F6       := WO~1~1~1.
+    Definition Minor_FENCE   := WO~0~0~0.
+    Definition Minor_FENCE_I := WO~0~0~1.
+    Definition Unused_F1     := WO~0~1~0.
+    Definition Unused_F2     := WO~0~1~1.
+    Definition Unused_F3     := WO~1~0~0.
+    Definition Unused_F4     := WO~1~0~1.
+    Definition Unused_F5     := WO~1~1~0.
+    Definition Unused_F6     := WO~1~1~1.
 
-    (*                               ---fm-- --pred- --succ- *)
-    Definition FENCE_RW_RW     := WO~1~0~0~0~0~0~1~1~0~0~1~1.
+    (*                             ---fm-- --pred- --succ- *)
+    Definition FENCE_RW_RW   := WO~1~0~0~0~0~0~1~1~0~0~1~1.
 
 (* Records *)
 
@@ -140,11 +140,11 @@ Section Decoder.
     Open Scope kami_action.
     Definition Decode_action : ActionT ty DInst.
     exact(
-        let x := "test" in
+    (* Basic Fields      *)
         LET opcode       <- instr $[  6 :  2 ];
         LET funct3       <- instr $[ 14 : 12 ];
-        LET funct3m2     <- instr $[ 14 : 13 ];        (* TODO : Get rid of these one-use? *)
-        LET funct3m1     <- instr $[ 14 : 14 ];        (* TODO                             *)
+        LET funct3m1     <- instr $[ 14 : 14 ];        (* TODO : Get rid of these one-use? *)
+        LET funct3m2     <- instr $[ 14 : 13 ];        (* TODO                             *)
         LET funct3l2     <- instr $[ 13 : 12 ];        (* TODO                             *)
         LET rd           <- instr $[ 11 :  7 ];
         LET rs1          <- instr $[ 19 : 15 ];
@@ -154,23 +154,31 @@ Section Decoder.
         LET funct7s      <- instr $[ 25 : 25 ];        (* part of shamt in RV64 *)
         LET funct7z      <- {< (instr $[ 31 : 31 ]) ,  (* remainder of funct7   *)
                                (instr $[ 29 : 26 ]) >};
+
+    (* Basic Tests       *)
+        LET funct3m1_0   <- #funct3m1 == $$ WO~0;
+        LET funct3_not0  <- #funct3 != $$ WO~0~0~0;
         LET funct7a0     <- #funct7a == $$ WO~0;
         LET funct7z0     <- #funct7z == $$ (natToWord 5 0);
         LET funct7sz0    <- #funct7z0 && (#funct7s == $$ WO~0);
+
+    (* Immediates        *)
         LET i_imm        <- SignExtend 52 (instr $[ 31 : 20 ]);
         LET u_imm        <- SignExtend 32 ({< instr $[ 31 : 12 ] , $$ (natToWord 12 0) >});
         LET j_imm        <- SignExtend 43 ({<(instr$[31:31]),(instr$[19:12]),(instr$[20:20]),(instr$[30:21]),$$WO~0>});
         LET b_imm        <- SignExtend 51 ({<(instr$[31:31]),(instr$[7:7]),(instr$[30:25]),(instr$[11:8]),$$WO~0>});
         LET s_imm        <- SignExtend 52 ({< (instr $[ 31 : 25 ]) , (instr $[ 11 : 7 ]) >});
         LET z_imm        <- ZeroExtend 59 #rs1;        (* TODO : Verify ZEXT to 64 not 32               *)
+
+    (* Misc Signals      *)
         LET fm           <- instr $[ 31 : 28 ];
         LET pred         <- instr $[ 27 : 24 ];
         LET succ         <- instr $[ 23 : 20 ];
-        LET funct3m1_0   <- #funct3m1 == $$ WO~0;
         LET not_sr       <- #funct3 != $$ WO~1~0~1;
-        LET funct3_not0  <- #funct3 != $$ WO~0~0~0;
         LET not_add      <- #funct3_not0;
-        LET not_ecall    <- #funct3_not0;              (* Note that ECALL and EBREAK share funct3       *)
+        LET not_ecall    <- #funct3_not0;              (* Note that ECALL and EBREAK have same funct3   *)
+
+    (* Format Checks     *)
         LET OP_IMM_ok    <- #not_sr                    (* 0b?01 are the shift instructions              *)
                             || #funct7z0;              (* || #funct7sz0 in RV32                         *)
         LET OP_IMM_32_ok <- #not_sr
@@ -193,6 +201,8 @@ Section Decoder.
         LET MISC_MEM_ok  <- ((#fm == $$ (natToWord 4 0))
                              || ({<#fm,#pred,#succ>} == $$ FENCE_RW_RW) (* See pg 24 of the spec        *)
                             ) && #funct3m2 == $$ WO~0~0;
+
+    (* Exceptions        *)
         LET illegal      <- !( ((#opcode == $$ Major_LOAD) && #LOAD_ok)
                             || ((#opcode == $$ Major_MISC_MEM) && #MISC_MEM_ok)
                             || ((#opcode == $$ Major_OP_IMM) && #OP_IMM_ok)
@@ -215,6 +225,8 @@ Section Decoder.
                          (* || (#opcode == $$ Major_NMADD) *)
                          (* || (#opcode == $$ Major_OP_FP) *)
                             );
+
+    (* Return Structs    *)
         LET SYS_rs1      <- #not_ecall && #funct3m1_0;
         LET SYS_rd       <- #not_ecall;
         LET SYS_csr      <- ! #not_ecall;
@@ -279,7 +291,7 @@ Section Decoder.
                                                                "rs2?"    ::= $$ false            ;
                                                                "rd?"     ::= $$ true             ;
                                                                "csr?"    ::= $$ false            };
-                                $$ Major_OP_32     ::= STRUCT {"imm"     ::= #i_imm              ; (* OP : Note that imm doesn't matter here; [natToWord 64 0] would work just as well *)
+                                $$ Major_OP_32     ::= STRUCT {"imm"     ::= #i_imm              ; (* OP-32 : Note that imm doesn't matter here; [natToWord 64 0] would work just as well *)
                                                                "rs1?"    ::= $$ true             ;
                                                                "rs2?"    ::= $$ true             ;
                                                                "rd?"     ::= $$ true             ;
