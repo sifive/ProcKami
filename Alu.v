9bedf439758e290e6189f715cee167fd90c0d20a
Murali Vijayaraghavan <vmurali@sifive.com>
2018-11-28 16:11:06 -0800

    addentry

--- Alu.v
+++ Alu.v
@@ -1,71 +1,48 @@
-Require Import Kami.All InstFormat.
-
-Local Open Scope char_scope.
-
-Definition binDigitToNat (c : ascii) : option nat :=
-  match c with
-    | "0" => Some 0
-    | "1" => Some 1
-    | _   => None
-  end.
-
-Open Scope string_scope.
-
-Fixpoint readBinAux (s : string) (acc : nat) : option nat :=
-  match s with
-    | "" => Some acc
-    | String c s' =>
-      match binDigitToNat c with
-        | Some n => readBinAux s' (2 * acc + n)
-        | None => None
-      end
-  end.
-
-Definition readBinNat (s : string) : option nat := readBinAux s 0.
-
-Goal readBinNat "01" = Some 1.
-Proof. reflexivity. Qed.
-
-Definition forceOption A Err (o : option A) (err : Err) : match o with
-                                                            | Some _ => A
-                                                            | None => Err
-                                                          end :=
-  match o with
-    | Some a => a
-    | None => err
-  end.
-
-Inductive parseError := ParseError.
-
-Definition bin (s : string) := @forceOption nat parseError (readBinNat s) ParseError.
-
-Notation "sz ''b' a" := (natToWord sz (bin a)) (at level 50).
-
-Notation "''b' a" := (natToWord _ (bin a)) (at level 50).
-
-Goal 'b"00001010" = WO~0~0~0~0~1~0~1~0.
-Proof. reflexivity. Qed.
-
-Goal 'b"1000001" = WO~1~0~0~0~0~0~1.
-Proof. reflexivity. Qed.
-
-Local Close Scope char_scope.
-
+Require Import Kami.All FU.
 
 Section Alu.
-  Variable n: nat.
+  Variable Xlen_over_8: nat.
+
+  Notation Xlen := (8 * Xlen_over_8).
+
+  Notation Data := (Bit Xlen).
+  Notation VAddr := (Bit Xlen).
+  Notation DataMask := (Bit Xlen_over_8).
   Variable ty: Kind -> Type.
 
-  Definition AluType := STRUCT {"arg1" :: Bit n ; "arg2" :: Bit n}.
+  Definition AluType := STRUCT {"arg1" :: Data ; "arg2" :: Data}.
 
   Local Open Scope kami_expr.
-  Definition AluEntry :=
-    {| fuName := "alu" ;
-       instEntries :=
-         (Build_InstEntry "addi" (IType ('b"0010011") ('b"000"))
-                          (fun i =>
-                             LETE x: AluType <- i;
-                             RetE ((#x @% "arg1") + (#x @% "arg2")))) :: nil
+  Definition AddEntry: FUEntry Xlen_over_8 ty :=
+    {| fuName := "add" ;
+       fuFunc := (fun i =>
+                    LETE x: AluType <- i;
+                      RetE ((#x @% "arg1") + (#x @% "arg2"))) ;
+       fuInsts :=
+         {| instName := "addi" ;
+            uniqId   := (Normal, (existT _ (6, 2) ('b"00100"))
+                                   :: (existT _ (14, 12) ('b"000")) :: nil) ;
+            inputXform :=
+              (fun gcpin =>
+                 LETE gcp: GenContextPkt Xlen_over_8 <- gcpin ;
+                   RetE ((STRUCT {
+                              "arg1" ::= #gcp @% "reg1" ;
+                              "arg2" ::= SignExtendTruncLsb
+                                           Xlen ((#gcp @% "instNoOpcode")$[24:13])
+                         }): AluType @# _)
+              ) ;
+            outputXform :=
+              (fun outarg =>
+                 LETE out: Data <- outarg ;
+                   RetE ((STRUCT {
+                              "tag"        ::= $IntInst ;
+                              "val1"       ::= #out ;
+                              "val2"       ::= $0 ;
+                              "memOp"      ::= $0 ;
+                              "memBitMask" ::= $0 ;
+                              "exception"  ::= invalidException _
+                         }): GenContextUpdPkt Xlen_over_8 @# ty));
+            optLoadXform := None |} :: nil
     |}.
   Local Close Scope kami_expr.
 End Alu.
