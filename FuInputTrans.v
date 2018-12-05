d2a09a09dadf750211b400d2d4ca558d31162db0
Larry D. Lee Jr <llee454@gmail.com>
2018-12-05 13:53:32 -0800

    Revised the Transformer generator in FuInputTrans and added a placeholder definition for trans_optional_packet_enabled.

--- FuInputTrans.v
+++ FuInputTrans.v
@@ -1,33 +1,25 @@
-(*
-  TODO: Replace the optional packet type with the Maybe kind
-*)
 Require Import Kami.All.
 Import Syntax.
 Require Import FU.
+Require Import InstMatcher. (* TODO: remove once trans_optional_packet_enabled has been revised. *)
 
 Section input_trans.
+
+Open Scope kami_expr.
 
 Variable Xlen_over_8 : nat.
 
 Variable ty : Kind -> Type.
 
-Definition raw_inst_type : Type := LetExprSyntax ty Inst.
+Let raw_inst_type : Type := LetExprSyntax ty Inst.
 
-Definition func_unit_entry_type : Type := @FUEntry Xlen_over_8 ty.
+Let func_unit_entry_type : Type := @FUEntry Xlen_over_8 ty.
 
-Definition inst_entry_type (sem_input_kind sem_output_kind : Kind)
+Let inst_entry_type (sem_input_kind sem_output_kind : Kind)
   := @InstEntry Xlen_over_8 ty sem_input_kind sem_output_kind.
 
-Definition exec_context_packet_kind : Kind
+Let exec_context_packet_kind : Kind
   := ExecContextPkt Xlen_over_8.
-
-Definition exec_context_packet_type : Type
-  := exec_context_packet_kind @# ty.
-
-Definition exec_context_packet_expr_type : Type
-  := exec_context_packet_kind ## ty.
-
-Open Scope kami_expr.
 
 Definition optional_packet
   (packet_type : Kind)
@@ -40,16 +32,28 @@ Definition optional_packet
          "data"  ::= input_packet
        }).
 
-Variable trans_optional_packet_enabled
-  : forall sem_input_kind sem_output_kind : Kind,
-    inst_entry_type sem_input_kind sem_output_kind ->
-    exec_context_packet_expr_type ->
-    LetExprSyntax ty Bool.
+(*
+  TODO: replace this is redundant as the Decoder already performs
+  instruction matching.
+*)
+Definition trans_optional_packet_enabled
+  (sem_input_kind sem_output_kind : Kind)
+  (inst_entry : inst_entry_type sem_input_kind sem_output_kind)
+  (exec_context_packet : exec_context_packet_kind ## ty)
+  :  LetExprSyntax ty Bool
+  := let exts
+       : list Extension
+       := nil in
+     LETE packet : exec_context_packet_kind <- exec_context_packet;
+      raw_inst_match_inst
+        exts
+        (RetE ((#packet) @% "inst"))
+        inst_entry.
 
 Definition trans_inst
   (sem_input_kind sem_output_kind : Kind)
   (inst_entry : inst_entry_type sem_input_kind sem_output_kind)
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   :  Maybe sem_input_kind ## ty
   := LETE packet : sem_input_kind <- inputXform inst_entry exec_context_packet;
      LETE enabled : Bool <-
@@ -58,16 +62,14 @@ Definition trans_inst
          exec_context_packet;
      (optional_packet (#packet) (#enabled)).
 
-Close Scope kami_expr.
-
 Definition trans_insts_aux (sem_input_kind sem_output_kind : Kind)
   (inst_entries : list (inst_entry_type sem_input_kind sem_output_kind))
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   :  Bit (size (Maybe sem_input_kind)) ## ty
   := list_rect
        (fun _
          => Bit (size (Maybe sem_input_kind)) ## ty)
-       (RetE (Const ty (wzero _)))%kami_expr
+       (RetE (Const ty (wzero _)))
        (fun inst_entry inst_entries (F : _)
          => LETE inst_entry_packet
               :  Maybe sem_input_kind
@@ -81,30 +83,30 @@ Definition trans_insts_aux (sem_input_kind sem_output_kind : Kind)
                   (ITE (ReadStruct (#inst_entry_packet) Fin.F1)
                     (pack (#inst_entry_packet))
                     $0)
-                  (cons (#insts_entry_packet_bstring) nil))))%kami_expr
+                  (cons (#insts_entry_packet_bstring) nil))))
        inst_entries.
 
 Definition trans_insts (sem_input_kind sem_output_kind : Kind)
   (inst_entries : list (inst_entry_type sem_input_kind sem_output_kind))
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   :  Maybe sem_input_kind ## ty
-  := (LETE packet_bstring
+  := LETE packet_bstring
        :  Bit (size (Maybe sem_input_kind))
        <- trans_insts_aux inst_entries exec_context_packet;
      RetE
        (unpack
          (Maybe sem_input_kind)
-         (#packet_bstring)))%kami_expr.
+         (#packet_bstring)).
 
 Fixpoint trans_func_unit
   (func_unit : func_unit_entry_type)
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   :  Maybe (fuInputK func_unit) ## ty
   := trans_insts (fuInsts func_unit) exec_context_packet.
 
 Fixpoint trans_func_units_vec
   (func_units : list func_unit_entry_type)
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   :  Vector.t ({entry_type : prod string Kind & LetExprSyntax ty (snd entry_type)}) (length func_units)
   := match func_units return 
        (Vector.t ({entry_type : prod string Kind & LetExprSyntax ty (snd entry_type)})
@@ -122,7 +124,7 @@ Fixpoint trans_func_units_vec
 
 Definition trans_func_units_struct
   (func_units : list func_unit_entry_type)
-  (exec_context_packet : exec_context_packet_expr_type)
+  (exec_context_packet : exec_context_packet_kind ## ty)
   := gatherLetExprVector
        (trans_func_units_vec func_units exec_context_packet).
 
@@ -157,5 +159,7 @@ Definition func_unit_output_kind
        | Some func_unit
          => Some (fuOutputK func_unit)
      end.
+
+Close Scope kami_expr.
 
 End input_trans.
