e410a72b13f40baa78d56b21c5163f5b53743892
Larry Lee <llee454@gmail.com>
2019-06-24 13:52:34 -0700

    [UNSTABLE] Debugging

--- FuncUnits/VirtualMem.v
+++ FuncUnits/VirtualMem.v
@@ -42,7 +42,6 @@ Section pt_walker.
     Variable satp_ppn: PAddr @# ty.
     Variable access_type: VmAccessType @# ty.
     Variable vAddr: VAddr @# ty.
-    Local Definition page_size := 12. (* num page bytes = 2^12 *)
 
     Definition PteFlags
       := STRUCT_TYPE {
@@ -102,7 +101,7 @@ Section pt_walker.
         Local Definition getVpnOffset: Bit VpnWidth ## ty :=
           RetE (satp_select
             (fun x
-              => ((vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - currentLevel) * vm_mode_vpn_size x)%nat) &
+              => ((vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - 2 - currentLevel) * vm_mode_vpn_size x)%nat) &
                 (ZeroExtendTruncLsb _
                   ($$(wones (vm_mode_vpn_size x))))) << wordOfShiftAmt (vm_mode_shift_num x))).
           
@@ -158,6 +157,23 @@ Section pt_walker.
           LETE leafVal: Maybe PAddr <- translatePteLeaf;
           LETE vpnOffset <- getVpnOffset;
           SystemE [
+            DispString _ ("[translatePte] current level: " ++ natToHexStr currentLevel ++ "\n");
+            DispString _ "[translatePte] vpn: ";
+            DispHex vpn;
+            DispString _ "\n";
+            (* DispString _ ("[translatePte] number of vpn fields: " ++ natToHexStr (length (vm_mode_sizes x)) ++ "\n"); *)
+            DispString _ "[translatePte] vpn 0: ";
+            DispHex (satp_select (fun x => vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - 0) * vm_mode_vpn_size x)%nat));
+            DispString _ "\n";
+            DispString _ "[translatePte] vpn 1: ";
+            DispHex (satp_select (fun x => (vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - 1) * vm_mode_vpn_size x)%nat)));
+            DispString _ "\n";
+            DispString _ "[translatePte] vpn 2: ";
+            DispHex (satp_select (fun x => (vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - 2) * vm_mode_vpn_size x)%nat)));
+            DispString _ "\n";
+            DispString _ "[translatePte] vpn 3: ";
+            DispHex (satp_select (fun x => (vpn >> wordOfVAddrShifter ((length (vm_mode_sizes x) - 3) * vm_mode_vpn_size x)%nat)));
+            DispString _ "\n";
             DispString _ "[translatePte] vpn offset: ";
             DispHex #vpnOffset;
             DispString _ "\n";
@@ -167,7 +183,7 @@ Section pt_walker.
           ];
           LETC nonLeafVal: Maybe PAddr <- STRUCT { "valid" ::= $$ true ;
                                                    "data" ::= ((ZeroExtendTruncLsb PAddrSz (pte @% "pointer") +
-                                                               ZeroExtendTruncLsb PAddrSz #vpnOffset) << (Const ty (natToWord 4 page_size))) } ;
+                                                               ZeroExtendTruncLsb PAddrSz #vpnOffset)) } ;
           LETC retVal: Maybe PAddr <- IF #leaf then #leafVal else #nonLeafVal;
           LETC finalVal: Pair Bool (Maybe PAddr) <- STRUCT { "fst" ::= ((!#validEntry) || #leaf) ;
                                                              "snd" ::= #retVal } ;
@@ -185,6 +201,7 @@ Section pt_walker.
               :  Maybe Data
               <- pMemRead (mem_read_index + currentLevel) mode (acc @% "snd" @% "data");
             System [
+              DispString _ "[translatePteLoop] ===================================: ";
               DispString _ "[translatePteLoop] pte: ";
               DispHex (#read_result @% "data");
               DispString _ "\n";
@@ -219,7 +236,7 @@ Section pt_walker.
         => LETA acc_result <- acc;
         translatePteLoop currentLevel #acc_result) (seq 0 maxPageLevels)
       (Ret (STRUCT { "fst" ::= $$ false ;
-                     "snd" ::= Valid (satp_ppn << (Const ty (natToWord 4 page_size)))}));
+                     "snd" ::= Valid (satp_ppn << (Const ty (natToWord 4 LgPageSizeLgPageSize)))}));
       System [
         DispString _ "[pte_translate] the resulting paddr: ";
         DispHex (#result @% "snd");
