2d8397ef170f6060c893871d8efab767fb507d43
Larry Lee <llee454@gmail.com>
2019-09-03 12:20:56 -0700

    Issue 88: Added support for multibyte UART accesses.

--- Devices/UARTDevice.v
+++ Devices/UARTDevice.v
@@ -11,10 +11,17 @@ Section device.
   Open Scope kami_expr.
   Open Scope kami_action.
 
+  Local Definition UARTRead
+    := STRUCT_TYPE {
+         "addr" :: Bit lgMemSz;
+         "size" :: MemRqLgSize
+       }.
+
   Local Definition UARTWrite
     := STRUCT_TYPE {
          "addr" :: Bit lgMemSz;
-         "data" :: Bit 8 (* every UART interface register is one byte wide *)
+         "data" :: Data; (* every UART interface register is one byte wide *)
+         "size" :: MemRqLgSize
        }.
 
   Definition uartDevice
@@ -22,34 +29,31 @@ Section device.
     := {|
          mem_device_name := "uart device";
          mem_device_type := io_device;
-         mem_device_pmas (* only byte access is supported *)
-           := {|
-                 pma_width      := 0;
-                 pma_readable   := true;
-                 pma_writeable  := true;
-                 pma_executable := true;
-                 pma_misaligned := true;
-                 pma_lrsc       := true;
-                 pma_amo        := AMOArith
-               |} ::
-               (map
-                 (fun width
-                   => {|
-                        pma_width      := width;
-                        pma_readable   := false;
-                        pma_writeable  := false;
-                        pma_executable := false;
-                        pma_misaligned := false;
-                        pma_lrsc       := false;
-                        pma_amo        := AMONone
-                      |})
-                 (seq 1 4));
+         mem_device_pmas
+           := (map
+                (fun width
+                  => {|
+                       pma_width      := width;
+                       pma_readable   := true;
+                       pma_writeable  := true;
+                       pma_executable := false;
+                       pma_misaligned := true;
+                       pma_lrsc       := true;
+                       pma_amo        := AMOArith
+                     |})
+                (seq 0 4));
          mem_device_read
            := fun ty
-                => [fun mode paddr _
-                     => Call result
-                          :  Bit 8
-                          <- ^"readUART" (SignExtendTruncLsb _ paddr : Bit lgMemSz);
+                => [fun mode paddr size
+                     => LET readRq
+                          :  UARTRead
+                          <- (STRUCT {
+                               "addr" ::= SignExtendTruncLsb lgMemSz paddr;
+                               "size" ::= size
+                             } : UARTRead @# ty);
+                        Call result
+                          :  Bit 64
+                          <- ^"readUART" (#readRq : UARTRead);
                         Ret (ZeroExtendTruncLsb Rlen #result)];
          mem_device_read_resv
            := fun ty _ addr _ => Ret $$ (getDefaultConst (Array Rlen_over_8 Bool));
@@ -62,7 +66,8 @@ Section device.
                           :  UARTWrite
                           <- (STRUCT {
                                "addr" ::= SignExtendTruncLsb lgMemSz (pkt @% "addr");
-                               "data" ::= unsafeTruncLsb 8 (pkt @% "data")
+                               "data" ::= pkt @% "data";
+                               "size" ::= pkt @% "size"
                              } : UARTWrite @# ty);
                         Call ^"writeUART" (#writeRq : _);
                         System [
