308ef3aa620b96040e7fae0ade30d143a416a7ef
Larry Lee <llee454@gmail.com>
2020-03-10 16:12:58 -0700

    Issue 267: removed extra comment.

--- MemOpsFuncs.v
+++ MemOpsFuncs.v
@@ -17,7 +17,7 @@ Section memOpsFuncs.
   Definition isMemRegValueFn (x : MemRegValue) : bool
     := match x with
        | memRegValueFn _ => true
-       | _ => false (* stores *)
+       | _ => false
        end.
 
   (* specifies the value written to memory by a memory operation. *)
